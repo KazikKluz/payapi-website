0d12c3fcf987f5e42948244fa322bb171909ed9a
Kazik Kluz <kazik.kluz@gmail.com>
2022-11-05 18:34:43 +0000

    about tablet done

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,21 +20,24 @@ const About: NextPage = () => {
                 className='font-serif text-sanJuanBlue text-[3.2rem] md:text-[4.8rem] 
              leading-[3.6rem] md:leading-[5.6rem] tracking-[-0.246154px] 
           md:tracking-[-0.369231px] mt-[4.8rem] md:mt-[7.5rem] xl:text-[5.6rem] xl:tracking-[-0.430769px]
-          h-[15.2rem]'
+          h-[15.2rem] md:w-[57.3rem] md:h-[17.8rem] md:mx-auto'
               >
                 We empower <br className='md:hidden' /> innovators <br /> by
                 delivering access to the financial system
               </h3>
 
               <div>
-                <div className='text-center'>
+                <div className='text-center md:text-left md:flex md:flex-row md:mt-[5.4rem] justify-between'>
                   <h4
                     className='font-serif text-sanJuanBlue text-[2.4rem] leading-[3.2rem]  tracking-[-0.184615px]
-                  mt-[4rem]'
+                  mt-[4rem] md:mt-0 md:text-[3.2rem] md:leading-[4rem] md:tracking-[-0.246154px] md:w-[23.3rem] md:text-left'
                   >
                     Our Vision
                   </h4>
-                  <p className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]'>
+                  <p
+                    className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]
+                  md:w-[45.6rem] md:mt-0'
+                  >
                     Our main goal is to build beautiful consumer experiences
                     along with developer-friendly infrastructure. The result is
                     an intelligent tool that gives everyone the ability to
@@ -43,14 +46,18 @@ const About: NextPage = () => {
                     technology.
                   </p>
                 </div>
-                <div>
+                <div className='text-center md:text-left md:flex md:flex-row md:mt-[6.4rem] justify-between'>
                   <h4
                     className='font-serif text-sanJuanBlue text-[2.4rem] leading-[3.2rem]  tracking-[-0.184615px]
-                  mt-[4rem]'
+                  mt-[4rem] md:mt-0 md:text-[3.2rem] md:leading-[4rem] md:tracking-[-0.246154px] md:w-[23.3rem] md:text-left
+                  '
                   >
                     Our Business
                   </h4>
-                  <p className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]'>
+                  <p
+                    className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]
+                  md:w-[45.6rem] md:mt-0'
+                  >
                     At the core of our platform is the technical infrastructure
                     APIs that connect consumers. Our innovative product provides
                     key insights for businesses and individuals, as well as
@@ -71,8 +78,22 @@ const About: NextPage = () => {
           alt='team'
         />
       </div>
-      <div className=' px-[2.4rem] mt-[4.8rem]'>
-        <div className='text-center border-t border-sanJuanBlue/25'>
+      <div className=' mx-auto max-w-[76.8rem] hidden md:inline-flex md:mt-[5.8rem]'>
+        <Image
+          src='/image-team-members-tablet.jpg'
+          width={769}
+          height={267}
+          alt='team'
+        />
+      </div>
+      <div
+        className=' px-[2.4rem] mt-[4.8rem]  md:flex md:flex-row md:justify-between md:h-[13.4rem]
+      md:px-[3.9rem]'
+      >
+        <div
+          className='text-center border-t border-sanJuanBlue/25 md:w-[22.3rem]
+        md:border-b md:text-left'
+        >
           <h5 className='font-sans text-[1.6rem] leading-[2.8rem] text-sanJuanBlue/70 mt-[1.6rem]'>
             Team Members
           </h5>
@@ -80,7 +101,10 @@ const About: NextPage = () => {
             300+
           </p>
         </div>
-        <div className='text-center'>
+        <div
+          className='text-center md:w-[22.3rem] md:border-t md:border-sanJuanBlue/25
+        md:border-b md:py-[1.6rem]  md:text-left'
+        >
           <h5 className='font-sans text-[1.6rem] leading-[2.8rem] text-sanJuanBlue/70'>
             Offices in the US
           </h5>
@@ -88,7 +112,10 @@ const About: NextPage = () => {
             3
           </p>
         </div>
-        <div className='text-center border-b border-sanJuanBlue/25 '>
+        <div
+          className='text-center border-b border-sanJuanBlue/25  md:w-[22.3rem]
+        md:border-t md:border-sanJuanBlue/25 md:py-[1.6rem] md:text-left'
+        >
           <h5 className='font-sans text-[1.6rem] leading-[2.8rem] text-sanJuanBlue/70'>
             Transactions analyzed
           </h5>
@@ -97,15 +124,19 @@ const About: NextPage = () => {
           </p>
         </div>
       </div>
-      <div className='text-center px-[2.4rem]'>
-        <div>
+      <div className='text-center px-[2.4rem] md:px-[3.9rem]'>
+        <div className='text-center md:text-left md:flex md:flex-row md:mt-[6.4rem] justify-between'>
           <h4
             className='font-serif text-sanJuanBlue text-[2.4rem] leading-[3.2rem]  tracking-[-0.184615px]
-                  mt-[4rem]'
+                  mt-[4rem] md:mt-0 md:text-[3.2rem] md:leading-[4rem] md:tracking-[-0.246154px] md:w-[23.3rem] md:text-left
+                  '
           >
             The Culture
           </h4>
-          <p className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]'>
+          <p
+            className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]
+                  md:w-[45.6rem] md:mt-0'
+          >
             We strongly believe there&apos;s always an opportunity to learn from
             each other outside of day-to-day work, whether it&apos;s
             company-wide offsites, internal hackathons, or co-worker meetups. We
@@ -113,21 +144,24 @@ const About: NextPage = () => {
             matter the job title.
           </p>
         </div>
-        <div>
+        <div className='text-center md:text-left md:flex md:flex-row md:mt-[6.4rem] justify-between'>
           <h4
             className='font-serif text-sanJuanBlue text-[2.4rem] leading-[3.2rem]  tracking-[-0.184615px]
-                  mt-[4rem]'
+                  mt-[4rem] md:mt-0 md:text-[3.2rem] md:leading-[4rem] md:tracking-[-0.246154px] md:w-[23.3rem] md:text-left'
           >
             The People
           </h4>
-          <p className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]'>
+          <p
+            className='font-sans text-lightSanJuanBlue text-[1.5rem] leading-[2.8rem] mt-[1.6rem]
+                  md:w-[45.6rem] md:mt-0'
+          >
             We&apos;re all passionate about building a more efficient and
             inclusive financial infrastructure together. At PayAPI, we have
             diverse backgrounds and skills.
           </p>
         </div>
       </div>
-      <div className='mt-[-3.2rem] mb-[14.3rem]'>
+      <div className='mt-[-3.2rem] mb-[14.3rem] md:mb-[10rem]'>
         <ReadyToStart />
       </div>
       <Footer />
